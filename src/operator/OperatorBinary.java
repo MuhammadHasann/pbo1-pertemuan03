2692fe47658d717ee3c739d5bf606aebf05d746b
MuhammadHasann <muhammadhas772@gmail.com>
2021-04-15 13:32:12 +0700

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -5,4 +5,11 @@ public class OperatorBinary {
     int jumlahSks = 24;
     double IPK = 3.89;
 
+    public static void main(String[] args) {
+        System.out.println(5+3);
+        System.out.println(5-3);
+        System.out.println(5/3);
+        System.out.println(5*3);
+        System.out.println(5%3);
+    }
 }
