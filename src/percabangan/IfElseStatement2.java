9c6fb8997492fffe089ee0dbe786670d48a2c385
MuhammadHasann <muhammadhas772@gmail.com>
2021-04-15 14:25:46 +0700

    Operator If Else Statement2

---
+++ src/percabangan/IfElseStatement2.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class IfElseStatement2 {
+    public static void main(String[] args) {
+        double uangBelanja = 3000;
+        double totalBelanja = 5000;
+
+        if(totalBelanja<uangBelanja){ //false
+            System.out.println("uang pas, bulik hati-hati kolo di rampok");
+        }else{
+            System.out.println("uang kurang, bulik kerumah minta duit pulang");
+        }
+    }
+}
